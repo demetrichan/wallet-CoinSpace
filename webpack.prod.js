c1689a504d71ac85c8b1ff927f9a2083a329c9e2
demetrichan <116949159+demetrichan@users.noreply.github.com>
2023-07-15 17:37:52 +0300

    Create webpack.prod.js

---
+++ webpack.prod.js
@@ -0,0 +1,3 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
