59bec7b771f93cfff1a1486b6e494dcae291c7ca
demetrichan <116949159+demetrichan@users.noreply.github.com>
2023-07-17 14:38:18 +0300

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,3 +3,4 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const autoprefixer = require('autoprefixer');
+const Dotenv = require('dotenv-webpack');
