a3ca0eccac9713782e795a77abe8f7d4ada36b2e
demetrichan <116949159+demetrichan@users.noreply.github.com>
2023-09-21 14:25:57 +0300

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,3 +2,4 @@ const Dotenv = require('dotenv-webpack');
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const autoprefixer = require('autoprefixer');
+const common = require('./webpack.common.js');
