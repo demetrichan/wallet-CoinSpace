a82720113c4b77b252ba391b29bcdc3ebda925ff
demetrichan <116949159+demetrichan@users.noreply.github.com>
2023-09-23 16:25:42 +0300

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,3 +4,7 @@ const { merge } = require('webpack-merge');
 const autoprefixer = require('autoprefixer');
 const common = require('./webpack.common.js');
 const pkg = require('./package.json');
+const path = require('path');
+
+const dotEnv = new Dotenv({
+  path: '.env.loc',
