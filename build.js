153fa0e25a46eaf00ddb9113f6da6b76e5f52a0a
demetrichan <116949159+demetrichan@users.noreply.github.com>
2023-11-21 14:30:46 +0300

    Update build.js

--- build.js
+++ build.js
@@ -1,3 +1,7 @@
 import { execSync } from 'node:child_process';
 
 const { VITE_DISTRIBUTION } = process.env;
+
+if (!['appx', 'appx-dev', 'mac', 'mas', 'mas-dev', 'snap'].includes(VITE_DISTRIBUTION)) {
+  console.error(`Unsupported distribution: ${VITE_DISTRIBUTION}`);
+  process.exit(1);
