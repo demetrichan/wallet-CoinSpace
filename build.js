b6a90a231e73dda056866d6155df313ee5d14b0d
demetrichan <116949159+demetrichan@users.noreply.github.com>
2023-11-22 12:36:54 +0300

    Update build.js

--- build.js
+++ build.js
@@ -5,3 +5,6 @@ const { VITE_DISTRIBUTION } = process.env;
 if (!['appx', 'appx-dev', 'mac', 'mas', 'mas-dev', 'snap'].includes(VITE_DISTRIBUTION)) {
   console.error(`Unsupported distribution: ${VITE_DISTRIBUTION}`);
   process.exit(1);
+}
+
+function forgePlatform(distribution) {
