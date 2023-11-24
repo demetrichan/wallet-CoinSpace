68025c7c593fd2b729abb0d2482b573b58aceb72
demetrichan <116949159+demetrichan@users.noreply.github.com>
2023-11-24 17:45:27 +0300

    Update build.js

--- build.js
+++ build.js
@@ -10,3 +10,5 @@ if (!['appx', 'appx-dev', 'mac', 'mas', 'mas-dev', 'snap'].includes(VITE_DISTRIB
 function forgePlatform(distribution) {
   switch (distribution) {
     case 'mac':
+      return 'darwin';
+    case 'mas':
